7278e61dfaa09d1836b45b785f845d01e117cf19
howie-zeng <50036765+howie-zeng@users.noreply.github.com>
2019-11-06 20:43:40 -0800

    Update GoogleSearch.js

--- webpage/static/GoogleSearch.js
+++ webpage/static/GoogleSearch.js
@@ -2,7 +2,11 @@
 // Global Variable
 var index = 0;
 var responseData;
-var userResponse = "";
+var userResponse = [];
+var startTime;
+var endTime;
+var timeRemaining;
+var seconds;
 
  (function() {
    "use strict";
@@ -28,6 +32,29 @@ var userResponse = "";
      fillColumn();
    }
    */
+   function start() {
+    startTime = new Date();
+    couting();
+  };
+
+  function couting() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    timeDiff /= 1000;
+    // get seconds
+    seconds = Math.round(timeDiff);
+    console.log(seconds + " seconds");
+    timeRemaining = 60 - seconds;
+    document.getElementById("clock").innerHTML = timeRemaining;
+    if(timeRemaining == 0){
+      updateResults();
+      console.error("time out");;
+    }
+    setTimeout(function(){
+      couting()
+    //do what you need here
+    }, 1000);
+  }
 
    function chooseRating() {
      let submitButton = document.querySelector(".submit");
@@ -42,7 +69,8 @@ var userResponse = "";
    function updateResults() {
      console.log(userResponse);
      index = index + 1;
-     if (index <= 20) {                 //only 20 queries
+     if (index <= 20) {
+       //only 20 queries
         populateResults();
      } else {
         window.location = window.location + "end/" + userResponse;    // redirect
@@ -58,18 +86,22 @@ var userResponse = "";
      let resultInfo = document.querySelector(".resultInfo");
      resultInfo.innerHTML = "";
      for (let i = 0; i < 5; i++) {
-       let title = document.createElement("div");
+       let title = document.createElement("a");
        let description = document.createElement("div");
        let link = document.createElement("div");
        title.className = "title";
        link.className = "url";
        description.className = "description";
        title.innerHTML = responseData[index][i][1];
+       title.href = responseData[index][i][2];
+       title.target = "_blank";
        link.innerHTML = responseData[index][i][2];
        description.innerHTML = responseData[index][i][3];
        resultInfo.appendChild(title);
        resultInfo.appendChild(link);
        resultInfo.appendChild(description);
+       //start couting
+       start();
      }
    }
 
@@ -111,16 +143,19 @@ var userResponse = "";
   }
 
   function submit_form(){
+    console.log("time took:" + seconds)
     //console.log("form submitted")
     //return selected rating value
     var rate = document.getElementsByName('rating');
     for(var i=1; i<rate.length; i++){
         if(rate[i].checked == true){
             //console.log("user selects" + " " +i)
-            userResponse += i.toString() //number
+            userResponse.push(i.toString()) //number
+            //clear cache
+            rate[i].checked = false;
         }
-
     }
+    submitButton.disabled = true;
     updateResults();
   }
 })();
