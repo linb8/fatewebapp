0b322d0dea9434faae6f910abbce8e3ca659cb9d
howie-zeng <50036765+howie-zeng@users.noreply.github.com>
2019-11-23 21:03:48 -0800

    naive version of models.py

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    age = models.CharField(max_length=20)
+    gender = models.CharField(max_length=20)
+    education = models.CharField(max_length=20)
+    ip_address = models.CharField(max_length=20, default="0.0.0.0")
+    mturk_id = models.CharField(max_length=50, default="None")
+    date_submitted = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+            return self.ip_address
