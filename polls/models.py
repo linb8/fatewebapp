f40ccf7ff4a7c8169bb0b0dd5f48a54b1c9747a2
howie-zeng <50036765+howie-zeng@users.noreply.github.com>
2019-11-23 21:12:47 -0800

    naive version of models.py

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class UserInfo(models.Model):
+    mturk_id = models.CharField(max_length=50, default="None")
+    ip_address = models.CharField(max_length=20, default="0.0.0.0")
     age = models.CharField(max_length=20)
     gender = models.CharField(max_length=20)
     education = models.CharField(max_length=20)
-    ip_address = models.CharField(max_length=20, default="0.0.0.0")
-    mturk_id = models.CharField(max_length=50, default="None")
     date_submitted = models.DateTimeField(auto_now_add=True)
     def __str__(self):
             return self.ip_address
