0623e8596e0716e464bb276853c42dc82dcbc170
Yukai <yanyukai1998@gmail.com>
2019-11-24 00:16:39 -0800

    Models.py: create models (database tables)

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,37 @@
 from django.db import models
 
+'''
+For more field information, check: https://docs.djangoproject.com/en/2.2/ref/models/fields/
+'''
+
+
+# ['age','gender','education level','ip address', 'browser-info', 'mturk-id']
 class UserInfo(models.Model):
-    mturk_id = models.CharField(max_length=50, default="None")
-    ip_address = models.CharField(max_length=20, default="0.0.0.0")
-    age = models.CharField(max_length=20)
-    gender = models.CharField(max_length=20)
-    education = models.CharField(max_length=20)
+    mturk_id = models.CharField(max_length=50, default="None", primary_key=True)
+    ip_address = models.CharField(max_length=50, default="0.0.0.0")
+    browser_info = models.CharField(max_length=50, default="None")
+    age = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    education = models.CharField(max_length=50)
+
+    # will set the timezone.now() only when the instance is created,
     date_submitted = models.DateTimeField(auto_now_add=True)
+
+    # For debugging purposes
     def __str__(self):
-            return self.ip_address
+            return "ID: " + self.mturk_id + ", Age: " + self. age + ", gender: " + self.gender + \
+                   ", Education: " + self.education
+
+
+# ['mturk-id','algorithm','query','rating','time spent']
+class Response(models.Model):
+    mturk_id = models.CharField(max_length=50, default="None")
+    algorithm = models.CharField(max_length=10)
+    query = models.CharField(max_length=100)
+    rating = models.PositiveSmallIntegerField()
+    time_spent = models.PositiveSmallIntegerField()
+
+    # For debugging purposes
+    def __str__(self):
+        return "ID: " + self.mturk_id + ", Algorithm: " + self.algorithm + ", Query: " + self.query + \
+               ", rating: " + self.rating.__str__() + ", timeSpent: " + self.time_spent.__str__()
